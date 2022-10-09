1c9b52f8561aba97a4f24de3a2be13b2a4e0fe7e
modupeadeonojobi <modupeadeonojobi@gmail.com>
2022-10-09 20:18:08 +0100

    WIP: add apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import BookList from './components/BookList';
+
 function App() {
-  return <div className='main'></div>;
+  return (
+    <div className='main'>
+      <h1>Nene's Reading List</h1>
+      <BookList />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookList = () => {
+  return (
+    <ul>
+      <li>One</li>
+    </ul>
+  );
+};
+
+export default BookList;
